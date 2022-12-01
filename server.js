32e169300df75fae77eb41f96d502ae4c5e480e6
Luis Delotavo <luisdelotavo@outlook.com>
2022-11-30 19:28:22 -0500

    Updated backend requirements

--- server.js
+++ server.js
@@ -13,13 +13,15 @@ app.get("/", (req, res) => {
 
 // Grabs a list of all the genres
 app.get("/genres", async (req, res) => {
+
+    // Data found is a cursor object and must be turned into an array
     let data = await coll.connectGenres();
     data = await data.find().toArray();
     res.send(data);
 });
 
 // Grabbing a specific artist with a given artist id
-app.get("/artists/:artist_id", async (req, res) => {
+app.get("/artistsNo/:artist_id", async (req, res) => {
     const id = req.params.artist_id;
     let data = await coll.connectArtists();
     data = await data.findOne(
@@ -33,7 +35,7 @@ app.get("/artists/:artist_id", async (req, res) => {
 });
 
 // Grabbing a specific track with a given track id
-app.get("/track/:track_id", async (req, res) => {
+app.get("/trackNo/:track_id", async (req, res) => {
     const id = req.params.track_id;
     let data = await coll.connectTracks();
     data = await data.findOne(
@@ -46,11 +48,62 @@ app.get("/track/:track_id", async (req, res) => {
     }
 });
 
-// Grabbing a maximum of 5 search results given a track/album name pattern
-app.get("/results/:pattern_id", async (req, res) => {
-    const id = req.params.pattern_id;
-    res.send(id);
+// Grabbing a maximum of 5 track ids given a track name pattern
+app.get("/trackName/:title", async (req, res) => {
+    const id = req.params.title;
+    let data = await coll.connectTracks();
+
+    // Using Regex for pattern matching
+    // Data found is a cursor object and must be turned into an array
+    data = await data.find(
+        { track_title: {$regex: id} } ).toArray();
+    data = data.slice(0,5);
+    
+    if (data) {
+        res.send(data);
+    }
+    else {
+        res.status(404).send(`No matches were found!`);
+    }
 })
+
+// Grabbing a maximum of 5 track ids given an album name pattern
+app.get("/albumName/:title", async (req, res) => {
+    const id = req.params.title;
+    let data = await coll.connectTracks();
+
+    // Using Regex for pattern matching
+    // Data found is a cursor object and must be turned into an array
+    data = await data.find(
+        { album_title: {$regex: id} } ).toArray();
+    data = data.slice(0,5);
+    
+    if (data) {
+        res.send(data);
+    }
+    else {
+        res.status(404).send(`No matches were found!`);
+    }
+});
+
+// Grabbing all the matching artist ids given an artist name pattern
+app.get("/artistName/:name", async (req, res) => {
+    const id = req.params.name;
+    let data = await coll.connectArtists();
+
+    // Using Regex for pattern matching
+    // Data found in a cursor object and must be turned into an array
+    data = await data.find(
+        { artist_name: {$regex: id} } ).toArray();
+    
+    if (data) {
+        res.send(data);
+    }
+    else {
+        res.status(404).send(`No matches were found!`);
+    }
+});
+
 
 
 // Listening to the port
